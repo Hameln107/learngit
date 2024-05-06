c5599d971ee44d88e90d0f0234369cbed631bcd0
leduylinh <intern.leduylinh@ecquaria.com>
2024-05-06 14:20:20 +0800

    Nothing

--- file2.c
+++ file2.c
@@ -1,51 +1,13 @@
 #include <stdio.h>
-#include <wchar.h>
-#include <windows.h>
-#include <stdbool.h>
+#include <math.h>
 
 
-int main() {
-	// Set output mode to handle virtual terminal sequences
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+int main() {	
 
-	if (hOut == INVALID_HANDLE_VALUE)
-		return false;
+	int arr[5] = {1, 2, 3, 4, 6};
 
-	HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
-
-	if (hIn == INVALID_HANDLE_VALUE)
-		return false;
-
-	DWORD dwOriginalOutMode = 0;
-	DWORD dwOriginalInMode = 0;
-
-	if (!GetConsoleMode(hOut, &dwOriginalOutMode))
-		return false;
-
-	if (!GetConsoleMode(hIn, &dwOriginalInMode))
-		return false;
-
-	DWORD dwRequestedOutModes = ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
-	DWORD dwRequestedInModes = ENABLE_VIRTUAL_TERMINAL_INPUT;
-	DWORD dwOutMode = dwOriginalOutMode | dwRequestedOutModes;
-
-	if (!SetConsoleMode(hOut, dwOutMode)) {
-		// we failed to set both modes, try to step down mode gracefully.
-		dwRequestedOutModes = ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-		dwOutMode = dwOriginalOutMode | dwRequestedOutModes;
-
-		if (!SetConsoleMode(hOut, dwOutMode)) {
-			// Failed to set any VT mode, can't do anything here.
-			return -1;
-		}
-	}
-
-	DWORD dwInMode = dwOriginalInMode | dwRequestedInModes;
-
-	if (!SetConsoleMode(hIn, dwInMode)) {
-		// Failed to set VT input mode, can't do anything here.
-		return -1;
-	}
+	for (int i = 0; i < 5; i++)
+		printf("%f\n", sqrt(arr[i]));
 
 	return 0;
 }
