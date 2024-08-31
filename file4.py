683bf90d3380593b64cecc14ba1bbe5c679ec24b
Hameln107 <91157888+Hameln107@users.noreply.github.com>
2024-08-31 18:17:18 +0800

    Change

--- file4.py
+++ file4.py
@@ -1,3 +1,13 @@
-print("Hello world")
-print("Changes in branch support")
-print("Lorem ipsum")
+import unittest
+
+def inc(x):
+	return x + 10
+
+class TestRearrange(unittest.TestCase):
+	def test_basic(self):
+		testcase = 11
+		expected = 20
+		self.assertEqual(inc(testcase), expected)
+
+# Run the tests
+unittest.main()
